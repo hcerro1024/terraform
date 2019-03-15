17f73b376c4af163e879c51a13990b91b12234a6
Henry Cerro <henry.cerro@gmail.com>
2019-03-15 17:26:30 +0000

    Changes - 2019-03-15:1326

--- main.tf
+++ main.tf
@@ -7,6 +7,22 @@ variable "server_port" {
     default = 8080
 }
 
+resource "aws_launch_configuration" {
+    image_id = "ami-40d28157"
+    instance_type = "t2.micro"
+    security_groups = ["${aws_security_group.instance.id}"]
+
+    user_data = <<-EOF
+		#!/bin/bash
+		echo "Hello, World" > index.html
+		nohup busybox httpd -f -p "${var.server_port}" &
+		EOF
+
+    lifecycle = {
+	create_before_destroy = true
+    }
+}
+
 resource "aws_instance" "example" {
     ami = "ami-40d28157"
     instance_type = "t2.micro"
@@ -31,6 +47,23 @@ resource "aws_security_group" "instance" {
 	to_port     = "${var.server_port}"
 	protocol    = "tcp"
 	cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    lifecycle = {
+	create_before_destroy = true
+    }
+}
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = "${aws_launch_configuration.example.id}"
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+	key = "Name"
+	value = "terraform-asg-example"
+	propagate_at_launch = true
     }
 }
 
