fd8c289205efcfd6acf117d1b08994bc95b83ae7
Henry Cerro <henry.cerro@gmail.com>
2019-03-19 18:53:10 +0000

    Changes - 2019-03-19:1453

--- main.tf
+++ main.tf
@@ -3,7 +3,16 @@ provider "aws" {
 }
 
 # Used in Autoscaling Group and ELB
-data "aws_availability_zones" "all" {}
+data "aws_availability_zones" "available" {}
+
+data "aws_subnet" "selected" {
+  id = "${var.subnet_id}"
+}
+
+data "aws_subnet" "example" {
+  count = "${length(data.aws_subnet_ids.example.ids)}"
+  id    = "${data.aws_subnet_ids.example.ids[count.index]}"
+}
 
 resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
@@ -18,6 +27,17 @@ resource "aws_security_group" "instance" {
     lifecycle = {
 	create_before_destroy = true
     }
+}
+
+resource "aws_security_group" "subnet" {
+  vpc_id = "${data.aws_subnet.selected.vpc_id}"
+
+  ingress {
+    cidr_blocks = ["${data.aws_subnet.selected.cidr_block}"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+  }
 }
 
 resource "aws_security_group" "elb" {
@@ -57,9 +77,9 @@ resource "aws_launch_configuration" "example" {
 
 resource "aws_autoscaling_group" "example" {
     launch_configuration = "${aws_launch_configuration.example.id}"
-    availability_zones = ["${data.aws_availability_zones.all.names}"]
+    availability_zones = ["${data.aws_availability_zones.available.names}"]
 
-    load_balancers = ["${aws_elb.example.name}"]
+    load_balancers = ["${aws_lb.example.name}"]
     health_check_type = "ELB"
 
     min_size = 2
@@ -72,10 +92,15 @@ resource "aws_autoscaling_group" "example" {
     }
 }
 
-resource "aws_elb" "example" {
+resource "aws_lb" "example" {
     name = "terraform-asg-example"
-    availability_zones = ["${data.aws_availability_zones.all.names}"]
-    security_groups = ["${aws_security_group.elb.id}"]
+	internal = false
+	load_balancer_type = "network"
+	bucket = "linkin-acloud-guru"
+	enabled = true
+	subnets            = "${element(data.aws_subnet_ids.example.ids, count.index)}"
+    availability_zones = ["${data.aws_availability_zones.available.names}"]
+    security_groups = ["${aws_security_group.subnet.id}"]
 
     listener {
 	lb_port = 80
@@ -91,8 +116,12 @@ resource "aws_elb" "example" {
 	interval = 30
 	target = "HTTP:${var.server_port}/"
     }
+	
+	tags = {
+		Environment = "training"
+	}	
 }
 
 output "elb_dns_name" {
-    value = "${aws_elb.example.dns_name}"
+    value = "${aws_lb.example.dns_name}"
 }
