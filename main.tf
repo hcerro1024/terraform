ec504d5b8549f44b3d57bb77644d6424ba64d45d
Henry Cerro <henry.cerro@gmail.com>
2019-03-21 18:12:32 +0000

    Reverted code back to Classic ELB - 2019-03-21:1412

--- main.tf
+++ main.tf
@@ -3,16 +3,7 @@ provider "aws" {
 }
 
 # Used in Autoscaling Group and ELB
-data "aws_availability_zones" "available" {}
-
-data "aws_subnet" "selected" {
-  id = "${var.subnet_id}"
-}
-
-data "aws_subnet" "example" {
-  count = "${length(data.aws_subnet_ids.example.ids)}"
-  id    = "${data.aws_subnet_ids.example.ids[count.index]}"
-}
+data "aws_availability_zones" "all" {}
 
 resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
@@ -27,17 +18,6 @@ resource "aws_security_group" "instance" {
     lifecycle = {
 	create_before_destroy = true
     }
-}
-
-resource "aws_security_group" "subnet" {
-  vpc_id = "${data.aws_subnet.selected.vpc_id}"
-
-  ingress {
-    cidr_blocks = ["${data.aws_subnet.selected.cidr_block}"]
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-  }
 }
 
 resource "aws_security_group" "elb" {
@@ -75,11 +55,27 @@ resource "aws_launch_configuration" "example" {
     }
 }
 
+/*
+resource "aws_instance" "example" {
+    ami = "ami-40d28157"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+    user_data = <<-EOF
+		#!/bin/bash
+		echo "Hello, World" > index.html
+		nohup busybox httpd -f -p "${var.server_port}" &
+		EOF
+    tags {
+	Name = "terraform-example"
+    }
+}
+*/
+
 resource "aws_autoscaling_group" "example" {
     launch_configuration = "${aws_launch_configuration.example.id}"
-    availability_zones = ["${data.aws_availability_zones.available.names}"]
+    availability_zones = ["${data.aws_availability_zones.all.names}"]
 
-    load_balancers = ["${aws_lb.example.name}"]
+    load_balancers = ["${aws_elb.example.name}"]
     health_check_type = "ELB"
 
     min_size = 2
@@ -92,15 +88,10 @@ resource "aws_autoscaling_group" "example" {
     }
 }
 
-resource "aws_lb" "example" {
+resource "aws_elb" "example" {
     name = "terraform-asg-example"
-	internal = false
-	load_balancer_type = "network"
-	bucket = "linkin-acloud-guru"
-	enabled = true
-	subnets            = "${element(data.aws_subnet_ids.example.ids, count.index)}"
-    availability_zones = ["${data.aws_availability_zones.available.names}"]
-    security_groups = ["${aws_security_group.subnet.id}"]
+    availability_zones = ["${data.aws_availability_zones.all.names}"]
+    security_groups = ["${aws_security_group.elb.id}"]
 
     listener {
 	lb_port = 80
@@ -116,12 +107,8 @@ resource "aws_lb" "example" {
 	interval = 30
 	target = "HTTP:${var.server_port}/"
     }
-	
-	tags = {
-		Environment = "training"
-	}	
 }
 
 output "elb_dns_name" {
-    value = "${aws_lb.example.dns_name}"
+    value = "${aws_elb.example.dns_name}"
 }
--- variables.tf
+++ variables.tf
@@ -2,5 +2,3 @@ variable "server_port" {
     description = "The port the server will use for HTTP requests"
     default = 8080
 }
-
-variable "subnet_id" {}
