06870f6407d3bec2626a54f0e68cd3099d4811e3
EC2 Default User <ec2-user@ip-172-31-93-42.ec2.internal>
2019-04-16 19:16:50 +0000

    Changes to user_data.sh - 2019-04-16:1516

--- main.tf
+++ main.tf
@@ -3,14 +3,6 @@ provider "aws" {
 }
 
 #------------------------------------------------------------------------
-# Key Pair
-#------------------------------------------------------------------------
-resource "aws_key_pair" "mykeypair" {
-  key_name   = "MyTestKeyPair2"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3/5sVAhbLFcrMLjKt2Opw+084HfBIpm58E1Nb+2kcrRwI93R8rb+jAOoDCcOlDw6SRwMFoDgA4CI6HO8Fb+V+28TS6UvZe3PgD6r0hwKcEXujy8PdLeRlDi098tkpJh/7l8r8GL0hnfb/pdWXg6qGAWfhwkB21yI2X/r/fjoQz2jMPiDe7B5OeljoNB5XZ6okAnz2iHop1bQQCk+TCsGoAIl1tFTwYDgjKo1oeCkz3OnYfzMfKvJv/QocVfxmYKFRIzEAqn/wQAT2bgmMce5eUKm1a9IxqPQp1KJCymR7jUUvoM1FJtLyVGlrgGaNgZOUOTOiuNTxk/4It0UTMCbB"
-}
-
-#------------------------------------------------------------------------
 # Data
 #------------------------------------------------------------------------
 # Used in Autoscaling Group and ELB
@@ -22,7 +14,7 @@ data "aws_availability_zones" "all" {}
 #
 # Security Group Rules
 #
-/*
+
 resource "aws_security_group_rule" "allow_http" {
   type            = "ingress"
   from_port       = 80
@@ -32,7 +24,6 @@ resource "aws_security_group_rule" "allow_http" {
 
   security_group_id = "${aws_security_group.launch_config.id}"
 }
-*/
 
 resource "aws_security_group_rule" "allow_ssh" {
   type            = "ingress"
@@ -43,6 +34,7 @@ resource "aws_security_group_rule" "allow_ssh" {
 
   security_group_id = "${aws_security_group.launch_config.id}"
 }
+
 #
 # Security Groups
 #
@@ -68,103 +60,14 @@ resource "aws_security_group" "launch_config" {
     }
 }
 
-/*
-#
-# This Security Group is for the Elastic Load Balancer
-#
-resource "aws_security_group" "elb" {
-    name = "terraform-example-elb"
-    
-    ingress {
-	from_port = "${var.standard_port}"
-	to_port = "${var.standard_port}"
-	protocol = "tcp"
-	cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-	from_port = 0
-	to_port = 0
-	protocol = "-1"
-	cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-
-#
-# Instances (Using a Launch Configuration since we don't yet have Launch Templates)
-#
-resource "aws_launch_configuration" "example" {
-    image_id = "ami-40d28157"
-    instance_type = "t2.micro"
-    security_groups = ["${aws_security_group.launch_config.id}"]
-
-    user_data = "${file("user_data.sh")}"
-    key_name = "MyTestKeyPair"
-
-    lifecycle = {
-	create_before_destroy = true
-    }
-}
-*/
 resource "aws_instance" "example" {
     instance_type = "t2.micro"
     ami = "ami-40d28157"
-    security_groups = ["${aws_security_group.launch_config.id}"]
+    security_groups = ["${aws_security_group.launch_config.name}"]
 
     user_data = "${file("user_data.sh")}"
-    key_name = "MyTestKeyPair (user data all commands"
-}
-
-/*
-#
-# Auto Scaling Group
-#
-resource "aws_autoscaling_group" "example" {
-    launch_configuration = "${aws_launch_configuration.example.id}"
-    availability_zones = ["${data.aws_availability_zones.all.names}"]
-
-    health_check_type = "ELB"
-
-    min_size = 1
-    max_size = 2 
-
-    tag {
-	key = "Name"
-	value = "terraform-asg-example"
-	propagate_at_launch = true
+    key_name = "MyTestKeyPair"
+    tags = {
+        Name = "My Test Server"
     }
 }
-*/
-
-/*
-#
-# Elastic Load Balancer
-#
-resource "aws_elb" "example" {
-    name = "terraform-asg-example"
-    availability_zones = ["${data.aws_availability_zones.all.names}"]
-    security_groups = ["${aws_security_group.elb.id}"]
-
-    listener {
-	lb_port = "${var.standard_port}"
-	lb_protocol = "http"
-	instance_port = "${var.server_port}"
-	instance_protocol = "http"
-    }
-
-    health_check {
-	healthy_threshold = 2
-	unhealthy_threshold = 2
-	timeout = 3
-	interval = 30
-	target = "HTTP:${var.server_port}/"
-    }
-}
-
-#------------------------------------------------------------------------
-# Output
-#------------------------------------------------------------------------
-output "elb_dns_name" {
-    value = "${aws_elb.example.dns_name}"
-}
-*/
--- user_data.sh
+++ user_data.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+cat > /home/ec2-user/.ssh/authorized_keys << EOF
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3/5sVAhbLFcrMLjKt2Opw+084HfBIpm58E1Nb+2kcrRwI93R8rb+jAOoDCcOlDw6SRwMFoDgA4CI6HO8Fb+V+28TS6UvZe3PgD6r0hwKcEXujy8PdLeRlDi098tkpJh/7l8r8GL0hnfb/pdWXg6qGAWfhwkB21yI2X/r/fjoQz2jMPiDe7B5OeljoNB5XZ6okAnz2iHop1bQQCk+TCsGoAIl1tFTwYDgjKo1oeCkz3OnYfzMfKvJv/QocVfxmYKFRIzEAqn/wQAT2bgmMce5eUKm1a9IxqPQp1KJCymR7jUUvoM1FJtLyVGlrgGaNgZOUOTOiuNTxk/4It0UTMCbB
+EOF
 yum update -y
 yum install httpd -y
 service httpd start
