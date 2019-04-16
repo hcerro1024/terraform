0571d7248a6caf9bc54222dcc12f54f361e6c655
EC2 Default User <ec2-user@ip-172-31-93-42.ec2.internal>
2019-04-16 12:44:58 +0000

    Changes, commands in S3 - 2019-04-16:0844

--- main.tf
+++ main.tf
@@ -3,6 +3,14 @@ provider "aws" {
 }
 
 #------------------------------------------------------------------------
+# Key Pair
+#------------------------------------------------------------------------
+resource "aws_key_pair" "mykeypair" {
+  key_name   = "MyTestKeyPair2"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3/5sVAhbLFcrMLjKt2Opw+084HfBIpm58E1Nb+2kcrRwI93R8rb+jAOoDCcOlDw6SRwMFoDgA4CI6HO8Fb+V+28TS6UvZe3PgD6r0hwKcEXujy8PdLeRlDi098tkpJh/7l8r8GL0hnfb/pdWXg6qGAWfhwkB21yI2X/r/fjoQz2jMPiDe7B5OeljoNB5XZ6okAnz2iHop1bQQCk+TCsGoAIl1tFTwYDgjKo1oeCkz3OnYfzMfKvJv/QocVfxmYKFRIzEAqn/wQAT2bgmMce5eUKm1a9IxqPQp1KJCymR7jUUvoM1FJtLyVGlrgGaNgZOUOTOiuNTxk/4It0UTMCbB"
+}
+
+#------------------------------------------------------------------------
 # Data
 #------------------------------------------------------------------------
 # Used in Autoscaling Group and ELB
@@ -12,9 +20,33 @@ data "aws_availability_zones" "all" {}
 # Resources
 #------------------------------------------------------------------------
 #
+# Security Group Rules
+#
+/*
+resource "aws_security_group_rule" "allow_http" {
+  type            = "ingress"
+  from_port       = 80
+  to_port         = 80
+  protocol        = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = "${aws_security_group.launch_config.id}"
+}
+*/
+
+resource "aws_security_group_rule" "allow_ssh" {
+  type            = "ingress"
+  from_port       = 22
+  to_port         = 22
+  protocol        = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]          
+
+  security_group_id = "${aws_security_group.launch_config.id}"
+}
+#
 # Security Groups
 #
-resource "aws_security_group" "instance" {
+resource "aws_security_group" "launch_config" {
     name = "terraform-example-instance"
 
     ingress {
@@ -24,11 +56,22 @@ resource "aws_security_group" "instance" {
 	cidr_blocks = ["0.0.0.0/0"]
     }
 
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     lifecycle = {
 	create_before_destroy = true
     }
 }
 
+/*
+#
+# This Security Group is for the Elastic Load Balancer
+#
 resource "aws_security_group" "elb" {
     name = "terraform-example-elb"
     
@@ -53,15 +96,26 @@ resource "aws_security_group" "elb" {
 resource "aws_launch_configuration" "example" {
     image_id = "ami-40d28157"
     instance_type = "t2.micro"
-    security_groups = ["${aws_security_group.instance.id}"]
+    security_groups = ["${aws_security_group.launch_config.id}"]
 
     user_data = "${file("user_data.sh")}"
+    key_name = "MyTestKeyPair"
 
     lifecycle = {
 	create_before_destroy = true
     }
 }
+*/
+resource "aws_instance" "example" {
+    instance_type = "t2.micro"
+    ami = "ami-40d28157"
+    security_groups = ["${aws_security_group.launch_config.id}"]
 
+    user_data = "${file("user_data.sh")}"
+    key_name = "MyTestKeyPair (user data all commands"
+}
+
+/*
 #
 # Auto Scaling Group
 #
@@ -69,11 +123,10 @@ resource "aws_autoscaling_group" "example" {
     launch_configuration = "${aws_launch_configuration.example.id}"
     availability_zones = ["${data.aws_availability_zones.all.names}"]
 
-    load_balancers = ["${aws_elb.example.name}"]
     health_check_type = "ELB"
 
-    min_size = 2
-    max_size = 10
+    min_size = 1
+    max_size = 2 
 
     tag {
 	key = "Name"
@@ -81,7 +134,9 @@ resource "aws_autoscaling_group" "example" {
 	propagate_at_launch = true
     }
 }
+*/
 
+/*
 #
 # Elastic Load Balancer
 #
@@ -112,3 +167,4 @@ resource "aws_elb" "example" {
 output "elb_dns_name" {
     value = "${aws_elb.example.dns_name}"
 }
+*/
